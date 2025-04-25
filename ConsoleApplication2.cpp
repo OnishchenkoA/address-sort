a64e516bc313f1b8ab4cbb5cf115299e9684c40f
Onoshchenko Alyona <eco.makarova.mgb1@mail.ru>
2025-04-25 17:57:23 +0500

    123456789

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class address
+class Address
 {
 private:
 	std::string town;
@@ -14,20 +14,27 @@ private:
 	int house;
 	int apart;
 	std::string output_adress{};
+	char temp;
+
 public:
-	address(std::string town, std::string strit, int house, int apart)
+	Address(std::string town, std::string strit, int house, int apart)
 	{
 		this->town = town;
 		this->strit = strit;
 		this->house = house;
 		this->apart = apart;
+		this->temp = temp;
 	}
 
-	std::string* get_town(address* address, int N)
+	Address()
 	{
-		std::string* town = new std::string[N]{};
-		return town;
+		town = " ";
+		strit = " ";
+		house = 0;
+		apart = 0;
+		temp = ' ';
 	}
+
 
 	std::string get_output_address()
 	{
@@ -35,42 +42,50 @@ public:
 		return output_adress;
 	}
 
+
+	int get_town()
+	{
+		char t = town.at(0);
+		int t1 = (int)t;
+		return t1;
+	}
+};
+
 std::string* mass_addr(int N)
 {
 	std::string* addr = new std::string[N]{};
 	return addr;
 }
 
-void del(std::string* addr, int N)
+void del(Address* addr, int N)
 {
 	delete[]addr;
 	addr = NULL;
 }
 
-void sort(address* address, int N)
+void sort(Address* address, int N)
 {
 	for (int i = 0; i < N; i++)
 		for (int j = i; j < N - 1; j++)
 		{
-			
-			if (address[j].town > address[j + 1].town)
+			if (address[j].get_town() > address[j + 1].get_town())
 			{
-				address temp = adress[j];
+				Address temp = address[j];
 				address[j] = address[j + 1];
 				address[j + 1] = temp;
 			}
 		}
 }
 
-void out_adr(std::string* addr, int N)
+void out_adr(Address* address, int N)
 {
 	std::ofstream adr1("out.txt");
 	if (adr1.is_open())
 	{
 		adr1 << N << std::endl;
-		for (int i = N; i > 0; i--)
+		for (int i = 0; i <= N; i++)
 		{
-			adr1 << addr[i] << std::endl;
+			adr1 << address[i].get_output_address() << std::endl;
 
 		}
 	}
@@ -78,7 +93,6 @@ void out_adr(std::string* addr, int N)
 		std::cout << "Ошибка открытия файла!" << std::endl;
 	}
 	adr1.close();
-}
 };
 
 int main(int argc, char** argv)
@@ -88,20 +102,18 @@ int main(int argc, char** argv)
 	std::string strit;
 	int house = 0;
 	int apart = 0;
-	address adr2(town, strit, house, apart);
 	std::ifstream adr("in.txt");
 	int N = 0;
 	adr >> N;
-	std::string* addr = adr2.mass_addr(N);
-	for (int i = 1; i <= N; i++)
+	Address* addr = new Address[N];
+	for (int i = 0; i < N; i++)
 	{
 		adr >> town >> strit >> house >> apart;
-		addr[i] = addr[town, strit, house, apart];
-		addr[i] = adr2.get_output_address();
+		addr[i] = Address(town, strit, house, apart);
 	}
 	adr.close();
-	adr2.sort(adr2.get_town(addr, N), N);
-	adr2.out_adr(addr, N);
-	adr2.del(addr, N);
+	sort(addr, N);
+	out_adr(addr, N);
+	del(addr, N);
 }
 
